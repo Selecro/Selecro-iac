ec6f51480c60ac8e0944aae813c5870a5d7e37d1
Szotkowski <mikiszotkowski@protonmail.com>
2025-04-23 01:01:50 +0200

    fixes

--- scripts/run_production.sh
+++ scripts/run_production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#ansible-playbook -i inventory/production playbooks/setup.yml --ask-become-pass
+ansible-playbook -i inventory/production playbooks/setup.yml --ask-become-pass
 ansible-playbook -i inventory/production playbooks/deploy.yml --ask-become-pass
