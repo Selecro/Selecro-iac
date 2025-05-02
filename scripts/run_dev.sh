1a7587388cf9bad37b6a72b32a712510f87ccd97
Szotkowski <mikiszotkowski@protonmail.com>
2025-05-02 23:32:37 +0200

    a lot of changes

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #ansible-playbook -i inventory/dev playbooks/setup.yml --ask-become-pass
-ansible-playbook -i inventory/dev playbooks/deploy.yml --ask-become-pass
+ansible-playbook -i inventory/dev playbooks/deploy.yml --ask-become-pass -vvv
